1827d91ec91743d9a742e8186732d2df84a47e72
rashaharb <rhh44@mail.aub.edu>
2024-03-11 14:40:22 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Slider from "./components/Slider";
+import Featured from "./components/Featured";
+import Offer from "./components/Offer";
+
+export default function Home() {
+  return (
+    <main>
+      <Slider/>
+      <Featured/>
+      <Offer/>
+    </main>
+  );
+}
